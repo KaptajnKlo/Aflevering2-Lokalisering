90f560d320538fddeee31aaebcdc04e09ad3a59f
KaptajnKlo <elisa.leineweber@gmail.com>
2022-10-13 12:05:40 +0200

    init

--- Opgave 7.py
+++ Opgave 7.py
@@ -1,5 +1,6 @@
 import pyomo.environ as pyomo  # Used for modelling the IP
 import readAndWriteJson as rwJson  # Used to read data from Json file
+from termcolor import colored
 
 def readData(filename: str) -> dict:
     data = rwJson.readJsonFileToDictionary(filename)
@@ -22,12 +23,22 @@ def buildModel(data: dict) -> pyomo.ConcreteModel():
     model.AntalSygehuse = data['p']
     model.RangeAntalKommuner = range(0,len(model.kommuner))
     model.budget_begrænsning = 29849347500
+    model.a = data['cover_matrix']
+    model.b = data['b']
 
-    
-
-
-
-
+    # Define variables
+    model.y = pyomo.Var(model.RangeAntalKommuner, within=pyomo.Binary)
+    model.z = pyomo.Var(model.RangeAntalKommuner, within=pyomo.Binary)
+    # Define objective function
+    model.obj = pyomo.Objective(expr=sum(model.z[j] for j in model.RangeAntalKommuner), sense=pyomo.maximize)
+    # Add covering constraints
+    model.coveringCsts = pyomo.ConstraintList()
+    for j in model.RangeAntalKommuner:
+        model.coveringCsts.add(
+            expr=sum(model.a[i][j] * model.y[i] for i in model.RangeAntalKommuner) >= model.b * model.z[j])
+    # Add cardinality constraint
+    model.cardinality = pyomo.Constraint(expr=sum(model.y[i] for i in model.RangeAntalKommuner) <= model.AntalSygehuse)
+    return model
 
 def solveModel(model: pyomo.ConcreteModel()):
  # Define a solver
@@ -35,23 +46,22 @@ def solveModel(model: pyomo.ConcreteModel()):
     # Solve the model
     solver.solve(model, tee=True)
 
-
 def displaySolution(model: pyomo.ConcreteModel()):
     # Print optimal objective function value
     print('Optimal objective function value is', pyomo.value(model.obj))
     print('The following facilities are open:')
-    for i in model.facilityRange:
+    for i in model.RangeAntalKommuner:
         if pyomo.value(model.y[i]) == 1:
-            print(model.facilities[i], end=',')
+            print(model.kommuner[i], end=',')
     print('\nCustomers are covered as follows:')
-    for j in model.customerRange:
+    for j in model.RangeAntalKommuner:
         if pyomo.value(model.z[j]) == 1:
-            print(colored(model.customers[j], 'green'),end='->\t')
+            print(colored(model.kommuner[j], 'green'),end='->\t')
         else:
-            print(colored(model.customers[j], 'red'), end='->\t')
-        for i in model.facilityRange:
+            print(colored(model.kommuner[j], 'red'), end='->\t')
+        for i in model.RangeAntalKommuner:
             if model.a[i][j] == 1 and pyomo.value(model.y[i])==1:
-                print(model.facilities[i], end=',')
+                print(model.kommuner[i], end=',')
         print('')
     # Print the open facilities
 
